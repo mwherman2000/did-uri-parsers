68e2cf3b30a5d955ce26cdc3f79f1fa343c192fd
Michael Herman <mwherman2000@hotmail.com>
2019-04-09 17:07:11 -0700

    Initial commit

---
+++ did-uri-parsers/Rule_transformer_quotedvalue.cs
@@ -0,0 +1,123 @@
+/* -----------------------------------------------------------------------------
+ * Rule_transformer_quotedvalue.cs
+ * -----------------------------------------------------------------------------
+ *
+ * Producer : com.parse2.aparse.Parser 2.5
+ * Produced : Tue Apr 09 16:49:04 PDT 2019
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+using System;
+using System.Collections.Generic;
+
+sealed public class Rule_transformer_quotedvalue:Rule
+{
+  private Rule_transformer_quotedvalue(String spelling, List<Rule> rules) :
+  base(spelling, rules)
+  {
+  }
+
+  public override Object Accept(Visitor visitor)
+  {
+    return visitor.Visit(this);
+  }
+
+  public static Rule_transformer_quotedvalue Parse(ParserContext context)
+  {
+    context.Push("transformer-quotedvalue");
+
+    Rule rule;
+    bool parsed = true;
+    ParserAlternative b;
+    int s0 = context.index;
+    ParserAlternative a0 = new ParserAlternative(s0);
+
+    List<ParserAlternative> as1 = new List<ParserAlternative>();
+    parsed = false;
+    {
+      int s1 = context.index;
+      ParserAlternative a1 = new ParserAlternative(s1);
+      parsed = true;
+      if (parsed)
+      {
+        bool f1 = true;
+        int c1 = 0;
+        for (int i1 = 0; i1 < 1 && f1; i1++)
+        {
+          rule = Rule_DQUOTE.Parse(context);
+          if ((f1 = rule != null))
+          {
+            a1.Add(rule, context.index);
+            c1++;
+          }
+        }
+        parsed = c1 == 1;
+      }
+      if (parsed)
+      {
+        bool f1 = true;
+        int c1 = 0;
+        for (int i1 = 0; i1 < 1 && f1; i1++)
+        {
+          rule = Rule_transformer_stringvalue.Parse(context);
+          if ((f1 = rule != null))
+          {
+            a1.Add(rule, context.index);
+            c1++;
+          }
+        }
+        parsed = c1 == 1;
+      }
+      if (parsed)
+      {
+        bool f1 = true;
+        int c1 = 0;
+        for (int i1 = 0; i1 < 1 && f1; i1++)
+        {
+          rule = Rule_DQUOTE.Parse(context);
+          if ((f1 = rule != null))
+          {
+            a1.Add(rule, context.index);
+            c1++;
+          }
+        }
+        parsed = c1 == 1;
+      }
+      if (parsed)
+      {
+        as1.Add(a1);
+      }
+      context.index = s1;
+    }
+
+    b = ParserAlternative.GetBest(as1);
+
+    parsed = b != null;
+
+    if (parsed)
+    {
+      a0.Add(b.rules, b.end);
+      context.index = b.end;
+    }
+
+    rule = null;
+    if (parsed)
+    {
+        rule = new Rule_transformer_quotedvalue(context.text.Substring(a0.start, a0.end - a0.start), a0.rules);
+    }
+    else
+    {
+        context.index = s0;
+    }
+
+    context.Pop("transformer-quotedvalue", parsed);
+
+    return (Rule_transformer_quotedvalue)rule;
+  }
+}
+
+/* -----------------------------------------------------------------------------
+ * eof
+ * -----------------------------------------------------------------------------
+ */
